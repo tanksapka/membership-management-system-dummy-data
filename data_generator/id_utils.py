7525d15952eab2c2a42d9c39e0089411c114b670
Tamas Schell <tanksapka@yahoo.com>
2022-03-07 23:57:36 +0100

    Fixing errors, adding support functions to id_utils.py

--- data_generator/id_utils.py
+++ data_generator/id_utils.py
@@ -1,37 +1,132 @@
 import pandas as pd
 import uuid
-from typing import Dict
+from itertools import count
+from typing import Dict, Iterable, Optional
 
-RELEVANT_FIELDS = {'id', 'person_id', 'organization_id'}
+
+def generate_uuid(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    df.insert(len(df.columns), col, df.index.to_series().apply(lambda x: uuid.uuid1()))
+    return df
+
+
+def generate_mapping_dfs(df_dict: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
+    relevant_keys = (
+        'person', 'organization', 'gender', 'membership_fee_category', 'address_type', 'phone_type', 'email_type'
+    )
+    return {key: generate_uuid(df[[]].copy(), f'{key}_uuid') for key, df in df_dict.items() if key in relevant_keys}
+
+
+def clean_up_id_columns(df: pd.DataFrame, indices: Optional[Iterable] = None) -> pd.DataFrame:
+    if indices:
+        column_order = iter(indices)
+    else:
+        column_order = count()
+    col: str
+    for col in df.columns:
+        if col.find('uuid') != -1:
+            df.insert(next(column_order), col, df.pop(col))
+            df.drop(columns=[col.replace('uuid', 'id')], inplace=True)
+            df.rename(columns={col: col.replace('uuid', 'id')})
+    return df
 
 
 def map_id_fields(df_dict: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
     result: Dict[str, pd.DataFrame] = {k: v.copy() for k, v in df_dict.items()}
-    mappings = dict()
+    mappings = generate_mapping_dfs(df_dict)
 
-    mappings['person'] = df_dict['person'][[]].copy()
-    mappings['person'].insert(len(mappings['person'].columns), 'uuid', uuid.uuid1())
-    mappings['organization'] = df_dict['organization'][[]].copy()
-    mappings['organization'].insert(len(mappings['organization'].columns), 'uuid', uuid.uuid1())
-    mappings['gender'] = df_dict['gender'][[]].copy()
-    mappings['gender'].insert(len(mappings['gender'].columns), 'uuid', uuid.uuid1())
-    mappings['membership_fee_category'] = df_dict['membership_fee_category'][[]].copy()
-    mappings['membership_fee_category'].insert(len(mappings['membership_fee_category'].columns), 'uuid', uuid.uuid1())
-    mappings['address_type'] = df_dict['address_type'][[]].copy()
-    mappings['address_type'].insert(len(mappings['address_type'].columns), 'uuid', uuid.uuid1())
-    mappings['phone_type'] = df_dict['phone_type'][[]].copy()
-    mappings['phone_type'].insert(len(mappings['phone_type'].columns), 'uuid', uuid.uuid1())
-    mappings['email_type'] = df_dict['email_type'][[]].copy()
-    mappings['email_type'].insert(len(mappings['email_type'].columns), 'uuid', uuid.uuid1())
+    result['person'] = result['person'].merge(
+        mappings['person'], how='left', left_index=True, right_index=True
+    )
+    result['person'].set_index('person_uuid', drop=True, inplace=True)
+    result['person_data'] = result['person_data'].merge(
+        mappings['person'], how='left', left_on='person_id', right_index=True
+    )
+    result['person_data'] = result['person_data'].merge(
+        mappings['gender'], how='left', left_on='gender_id', right_index=True
+    )
+    result['person_data'] = result['person_data'].merge(
+        mappings['membership_fee_category'], how='left', left_on='membership_fee_category_id', right_index=True
+    )
+    clean_up_id_columns(result['person_data'], (0, 7, 9))
 
-    result['person_data'] = result['person_data'].merge(mappings['person'], how='left', left_on='person_id',
-                                                        right_index=True)
-    result['person_data'] = result['person_data'].merge(mappings['gender'], how='left', left_on='gender_id',
-                                                        right_index=True, suffixes=('_gender', '_gender'))
-    result['person_data'] = result['person_data'].merge(mappings['membership_fee_category'], how='left',
-                                                        left_on='membership_fee_category_id', right_index=True,
-                                                        suffixes=('_membership_fee_category', '_membership_fee_category'))
+    result['organization'] = result['organization'].merge(
+        mappings['organization'], how='left', left_index=True, right_index=True
+    )
+    result['organization'].set_index('organization_uuid', drop=True, inplace=True)
     result['organization_data'] = result['organization_data'].merge(mappings['organization'], how='left',
                                                                     left_on='organization_id', right_index=True)
+    result['organization_data'] = result['organization_data'].merge(mappings['organization'], how='left',
+                                                                    left_on='organization_parent_id', right_index=True)
+    result['organization_data'].rename(columns={
+        'organization_uuid_x': 'organization_uuid',
+        'organization_uuid_y': 'organization_parent_uuid',
+    }, inplace=True)
+    clean_up_id_columns(result['organization_data'])
+
+    result['address'] = result['address'].merge(
+        mappings['person'], how='left', left_on='person_id', right_index=True
+    )
+    result['address'] = result['address'].merge(
+        mappings['organization'], how='left', left_on='organization_id', right_index=True
+    )
+    result['address'] = result['address'].merge(
+        mappings['address_type'], how='left', left_on='address_type_id', right_index=True
+    )
+    clean_up_id_columns(result['address'])
+
+    result['phone'] = result['phone'].merge(
+        mappings['person'], how='left', left_on='person_id', right_index=True
+    )
+    result['phone'] = result['phone'].merge(
+        mappings['organization'], how='left', left_on='organization_id', right_index=True
+    )
+    result['phone'] = result['phone'].merge(
+        mappings['phone_type'], how='left', left_on='phone_type_id', right_index=True
+    )
+    clean_up_id_columns(result['phone'])
+
+    result['email'] = result['email'].merge(
+        mappings['person'], how='left', left_on='person_id', right_index=True
+    )
+    result['email'] = result['email'].merge(
+        mappings['organization'], how='left', left_on='organization_id', right_index=True
+    )
+    result['email'] = result['email'].merge(
+        mappings['email_type'], how='left', left_on='email_type_id', right_index=True
+    )
+    clean_up_id_columns(result['email'])
+
+    result['membership'] = result['membership'].merge(
+        mappings['person'], how='left', left_on='person_id', right_index=True
+    )
+    result['membership'] = result['membership'].merge(
+        mappings['organization'], how='left', left_on='organization_id', right_index=True
+    )
+    clean_up_id_columns(result['membership'])
+
+    result['gender'] = result['gender'].merge(
+        mappings['gender'], how='left', left_index=True, right_index=True
+    )
+    result['gender'].set_index('gender_uuid', drop=True, inplace=True)
+
+    result['membership_fee_category'] = result['membership_fee_category'].merge(
+        mappings['membership_fee_category'], how='left', left_index=True, right_index=True
+    )
+    result['membership_fee_category'].set_index('membership_fee_category_uuid', drop=True, inplace=True)
+
+    result['address_type'] = result['address_type'].merge(
+        mappings['address_type'], how='left', left_index=True, right_index=True
+    )
+    result['address_type'].set_index('address_type_uuid', drop=True, inplace=True)
+
+    result['phone_type'] = result['phone_type'].merge(
+        mappings['phone_type'], how='left', left_index=True, right_index=True
+    )
+    result['phone_type'].set_index('phone_type_uuid', drop=True, inplace=True)
+
+    result['email_type'] = result['email_type'].merge(
+        mappings['email_type'], how='left', left_index=True, right_index=True
+    )
+    result['email_type'].set_index('email_type_uuid', drop=True, inplace=True)
 
     return result
